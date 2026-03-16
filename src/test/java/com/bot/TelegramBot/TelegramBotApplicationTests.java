a8e4d56f8d9739cba4623f0b8b5988b07ab59d8a
Jenya <borisjenya2@gmail.com>
2026-03-16 23:09:20 +0200

    First package v1.0

--- src/test/java/com/bot/TelegramBot/TelegramBotApplicationTests.java
+++ src/test/java/com/bot/TelegramBot/TelegramBotApplicationTests.java
@@ -2,8 +2,14 @@ package com.bot.TelegramBot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "BOT_TOKEN=test_token",
+        "DB_PASSWORD=QWER2404ty",
+        "spring.autoconfigure.exclude=org.telegram.telegrambots.starter.TelegramBotStarterConfiguration"
+})
 class TelegramBotApplicationTests {
 
 	@Test
